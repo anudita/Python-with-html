c97c7e555753e5b36418683a226037171dad5e3e
anudita <39922334+anudita@users.noreply.github.com>
2018-06-21 20:25:23 +0530

    Add files via upload
    Starting and stoping virtual machines using python and html for backend and front end respectively. This program is to automate the virtual machine starting and stoping on linux.

---
+++ vmstart.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import cgi
+import commands
+
+print "Content-type: text/html\n\n"
+data=cgi.FieldStorage()
+
+namenode=data.getvalue('nnode')
+datanode1=data.getvalue('dnode1')
+datanode2=data.getvalue('dnode2')
+datanode3=data.getvalue('dnode3')
+
+print namenode, datanode1, datanode2, datanode3
+if namenode == 'nnode':
+	commands.getoutput('sudo virsh start namenode')
+if datanode1 == 'dnode1':
+	commands.getoutput('sudo virsh start datanode1')
+if datanode2 == 'dnode2':
+	commands.getoutput('sudo virsh start datanode2')
+if datanode3 == 'dnode3':
+	commands.getoutput('sudo virsh start datanode3')
